e2c7c52bd39167c552462d851ca2567a6f466656
Maksym_Shveda <maksim1shveda@gmail.com>
2021-10-24 19:26:37 +0300

    calculator done

---
+++ src/components/clearbutton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './clearbutton.css';
+
+export const ClearButton = (props) =>(
+    <div className="clear-btn" onClick={props.handleClear}>
+        {props.handleClear}Clear
+    </div>
+)
---
+++ src/components/input.jsx
@@ -0,0 +1,7 @@
+import { prodDependencies } from 'mathjs';
+import react from 'react';
+import './input.css';
+
+export const Input = (props) => (
+ <div className="input">{props.input}</div>
+);
